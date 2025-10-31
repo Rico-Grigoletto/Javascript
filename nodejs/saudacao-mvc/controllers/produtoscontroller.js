2b4dac73e65c0bacdb26564e955df0e8ed14aec6
Enrico Grigoletto da Silva <enricogrisilva@gmail.com>
2025-10-31 16:15:13 -0300

    cadastrar produtos

--- nodejs/saudacao-mvc/controllers/produtoscontroller.js
+++ nodejs/saudacao-mvc/controllers/produtoscontroller.js
@@ -1,12 +1,20 @@
-const { formulario } = require("./siteController");
+const { formulario, camisetas } = require("./siteController");
+
+
 
 module.exports = {
         formulario, (req, res) => {
             res.sendFile('formulario.html', { root: './views' });
-
+            camisetas: (req, res) => {
+                res.sendFile('camisetas.html', { root: './views' });
+            },
+            formcadastrar: (req, res) => {
+                res.sendFile('formcadastrar.html', { root: './views' });
+            },
             cadastrar: (req, res) => {
                 const { nome, preco, ID, quant } = req.body;  
                 const mensagemcadastro = produtosmodel.gerarMensagemCadastro(nome, preco, ID, quant);
                 res.send(`<h1>${mensagemcadastro}</h1>`);
+            
 }
 };
